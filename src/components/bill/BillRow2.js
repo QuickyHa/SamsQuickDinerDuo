f826b7abc3f622d6186439cb136df71dbd7280ca
srmacam <saremacam@gmail.com>
2018-03-04 10:07:44 -0800

    done with bill

--- src/components/bill/BillRow2.js
+++ src/components/bill/BillRow2.js
@@ -2,7 +2,25 @@ import React from 'react';
 import {BillItem} from './BillItem';
 import {BillCount} from './BillCount';
 
+
+function moneyFormat(cost) { return "$" + parseFloat(Math.round(cost * 100) / 100).toFixed(2); }
+
 export class BillRow2 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { total: this.props.subtotal };
+        this.tip10 = this.tip10.bind(this);
+        this.tip15 = this.tip15.bind(this);
+        this.tip20 = this.tip20.bind(this);
+        this.tip0 = this.tip0.bind(this);
+    }
+
+    tip10() { this.setState({ total: this.props.subtotal*1.10 }); }
+    tip15() { this.setState({ total: this.props.subtotal*1.15 }); }
+    tip20() { this.setState({ total: this.props.subtotal*1.20 }); }
+    tip0() { this.setState({ total: this.props.subtotal }); }
+
+    paid() { window.location = "/"}
 
     render() {
         let itemList = this.props.countedOrders.map((ord) => <div key={ord.name}><BillItem name={ord.name}/><BillCount count={ord.count}/></div>);
@@ -18,11 +36,11 @@ export class BillRow2 extends React.Component {
                         <button className="tip" onClick={this.tip15}>Tip 15%</button>
                         <button className="tip" onClick={this.tip20}>Tip 20%</button>
                         <button className="tip" onClick={this.tip0}>No Tip </button>
-                        <h3 >Total:$0.00</h3>
+                        <h3 >Total: {moneyFormat(this.state.total)} </h3>
                     </div>
                     <div className ="billRow2 sub3">
-                        <button className="cash" onClick="paid()">CASH</button>
-                        <button className="card" onClick="paid()">CARD</button>
+                        <button className="cash" onClick={this.paid}>CASH</button>
+                        <button className="card" onClick={this.paid}>CARD</button>
                     </div>
                 </div>
             </div>
