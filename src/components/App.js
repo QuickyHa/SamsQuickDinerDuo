faf5b5ec07f9703aba284bf3ca005664c6ba4ca3
srmacam <saremacam@gmail.com>
2018-03-03 23:22:11 -0800

    AAAA

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, {PropTypes} from 'react';
 class App extends React.Component {
     render() {
         return (
-            <div className="everything">
+            <div className="container-fluid">
                 {this.props.children}
             </div>
         );
