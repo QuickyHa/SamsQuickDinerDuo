4c323c3eb60e9086fd4277da5c7538184fbdaf4e
srmacam <saremacam@gmail.com>
2018-03-04 08:31:57 -0800

    Partially done with chat

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,17 +1,45 @@
 import React from 'react';
 
 export class Chat extends React.Component{
-    message(){
+
+    constructor(props) {
+        super(props);
+        this.state = { inputValue: '' };
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
+
+    submit() {
+        this.props.onSubmit(this.state.inputValue);
+        this.setState({inputValue: ''});
+    }
+
+    handleKeyDown(event) {
+        if (event.keyCode === 13) {
+            this.submit();
+        }
+    }
+
+    handleChange(event) {
+        this.setState({inputValue: event.target.value});
+    }
+
     render(){
         return(
             <div className = "chat">
                 <form>
-                    <input className="text" type="text" id="msg" placeholder="Type your message here"
-                        onKeyDown = "if (event.keyCode==13) { message(); return false; }"/>
-                    <a className="reset" href="javascript:void(0)" onClick={this.message} id="send"> &#8594; </a>
+                    <input className="text" type="text" placeholder="Type your message here"
+                        value={this.state.inputValue}
+                        onChange={this.handleChange}
+                        onKeyDown={this.handleKeyDown}/>
+                    <a className="reset" href="javascript:void(0)" onClick={this.submit}> &#8594; </a>
                 </form>
             </div>
         );
     }
 }
+
+Chat.propTypes = {
+    onSubmit: React.PropTypes.func.isRequired
+};
--- src/components/chat/ChatPage.js
+++ src/components/chat/ChatPage.js
@@ -5,16 +5,31 @@ import {ChatContainerCustomer} from './ChatContainerCustomer';
 import {Chat} from './Chat';
 
 class ChatPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { chat: []};
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(msg) {
+        let newChat = this.state.chat;
+        newChat.push(msg);
+        this.setState({ chat: newChat });
+    }
+
     render() {
+        let chatList = null;
         return (
             <div id="chatContainer">
-                <ChatContainer />
-                <ChatContainerCustomer />
-                <Chat />
+                {chatList}
+                <Chat onSubmit={this.handleSubmit}/>
             </div>
         );
     }
 }
 
+ChatPage.propTypes = {
+    chat: React.PropTypes.array.isRequired,
+};
 
 export default ChatPage;
